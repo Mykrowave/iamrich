bed734f39f0d642964d7d93416e2e6933d3a2532
Mike Parker <mikeparker@crescerance.com>
2019-08-17 22:19:28 -0400

    scaffoldingaFlutterApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://media.giphy.com/media/h0MTqLyvgG0Ss/source.gif'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     ),
   );
